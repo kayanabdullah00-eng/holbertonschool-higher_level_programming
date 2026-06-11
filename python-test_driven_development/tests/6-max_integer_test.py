9f0f6bccf5789479f0a9332a233062e6ab671734
kayanabdullah00-eng <kayanabdullah00@gmail.com>
2026-06-11 23:04:14 +0000

    Add unittests for max_integer

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])"""
+
+import unittest
+
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """Test cases for max_integer"""
+
+    def test_empty_list(self):
+        """Test empty list"""
+        self.assertIsNone(max_integer([]))
+
+    def test_one_element(self):
+        """Test list with one element"""
+        self.assertEqual(max_integer([5]), 5)
+
+    def test_positive_integers(self):
+        """Test positive integers"""
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_max_at_beginning(self):
+        """Test max at beginning"""
+        self.assertEqual(max_integer([9, 3, 2, 1]), 9)
+
+    def test_max_in_middle(self):
+        """Test max in middle"""
+        self.assertEqual(max_integer([1, 8, 2, 3]), 8)
+
+    def test_negative_integers(self):
+        """Test negative integers"""
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+
+    def test_mixed_integers(self):
+        """Test mixed integers"""
+        self.assertEqual(max_integer([-10, 5, 2, -1]), 5)
+
+    def test_float_numbers(self):
+        """Test floats"""
+        self.assertEqual(max_integer([1.5, 3.2, 2.1]), 3.2)
+
+    def test_string(self):
+        """Test string sequence"""
+        self.assertEqual(max_integer("Bash"), "s")
+
+
+if __name__ == "__main__":
+    unittest.main()
