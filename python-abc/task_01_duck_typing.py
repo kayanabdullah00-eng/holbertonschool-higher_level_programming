0777170e30361a423045be46dd3803efa163f0dd
kayanabdullah00-eng <kayanabdullah00@gmail.com>
2026-06-26 20:30:02 +0300

    Fix duck typing task

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,30 +1,40 @@
 #!/usr/bin/python3
+"""Shapes, interfaces, and duck typing."""
+
 from abc import ABC, abstractmethod
 import math
 
 
 class Shape(ABC):
+    """Abstract shape class."""
+
     @abstractmethod
     def area(self):
+        """Return area."""
         pass
 
     @abstractmethod
     def perimeter(self):
+        """Return perimeter."""
         pass
 
 
 class Circle(Shape):
+    """Circle class."""
+
     def __init__(self, radius):
         self.radius = radius
 
     def area(self):
-        return math.pi * (self.radius ** 2)
+        return math.pi * self.radius ** 2
 
     def perimeter(self):
         return 2 * math.pi * self.radius
 
 
 class Rectangle(Shape):
+    """Rectangle class."""
+
     def __init__(self, width, height):
         self.width = width
         self.height = height
@@ -37,5 +47,6 @@ class Rectangle(Shape):
 
 
 def shape_info(shape):
-    print("Area:", shape.area())
-    print("Perimeter:", shape.perimeter())
+    """Print shape area and perimeter."""
+    print("Area: {}".format(shape.area()))
+    print("Perimeter: {}".format(shape.perimeter()))
