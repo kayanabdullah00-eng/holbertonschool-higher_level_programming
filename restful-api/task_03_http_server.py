df9a4225b6d1413c37701a380d24a2f7372d1703
kayanabdullah00-eng <kayanabdullah00@gmail.com>
2026-07-07 15:33:42 +0300

    Add simple HTTP server API

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""
+Simple API using Python http.server module.
+"""
+
+import json
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+class SimpleAPIHandler(BaseHTTPRequestHandler):
+    """Request handler for the simple API."""
+
+    def do_GET(self):
+        """Handle GET requests."""
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+
+        elif self.path == "/data":
+            data = {"name": "John", "age": 30, "city": "New York"}
+            response = json.dumps(data)
+
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json")
+            self.end_headers()
+            self.wfile.write(response.encode("utf-8"))
+
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"OK")
+
+        elif self.path == "/info":
+            info = {
+                "version": "1.0",
+                "description": "A simple API built with http.server"
+            }
+            response = json.dumps(info)
+
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json")
+            self.end_headers()
+            self.wfile.write(response.encode("utf-8"))
+
+        else:
+            self.send_response(404)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Endpoint not found")
+
+
+def run(server_class=HTTPServer, handler_class=SimpleAPIHandler):
+    """Start the HTTP server."""
+    server_address = ("", 8000)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    run()
