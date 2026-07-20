b957cc46ef22606c93e715639de03708f8eb2e37
kayanabdullah00-eng <kayanabdullah00@gmail.com>
2026-07-20 16:39:39 +0300

    k

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- This script creates a table called first_table in the current database
+-- Command to create a table with specific columns if it does not exist
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT ,
+    name VARCHAR(256)
+);
