9b1b957396e0b4a9ace6e6c1db35f5e5b06f5453
kayanabdullah00-eng <kayanabdullah00@gmail.com>
2026-07-21 17:00:08 +0300

    k

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,14 +1,11 @@
 -- This script creates a table second_table and inserts multiple rows
--- Command to create second_table if it does not exist
 CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
     score INT
 );
 
--- Command to insert initial records into second_table
-INSERT INTO second_table (id, name, score)
-    (1, 'John', 10),
-    (2, 'Alex', 3),
-    (3, 'Bob', 14),
-    (4, 'George', 8);
+INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
+INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 3);
+INSERT INTO second_table (id, name, score) VALUES (3, "Bob", 14);
+INSERT INTO second_table (id, name, score) VALUES (4, "George", 8);
