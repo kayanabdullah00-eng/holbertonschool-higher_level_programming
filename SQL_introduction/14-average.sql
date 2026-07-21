558494bb48b0c2bac2a142e5ba02575c67fe6f9b
kayanabdullah00-eng <kayanabdullah00@gmail.com>
2026-07-21 17:08:31 +0300

    k

---
+++ SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- This script computes the score average of all records in second_table
+-- Command to calculate average score and label the result column as average
+SELECT AVG(score) AS average FROM second_table;
