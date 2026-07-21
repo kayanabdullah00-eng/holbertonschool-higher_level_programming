7937f850610f9f229e7c7a9b5fc60f3ddd329673
kayanabdullah00-eng <kayanabdullah00@gmail.com>
2026-07-21 17:02:09 +0300

    k

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- This script lists all records of second_table ordered by score (top first)
+-- Command to select score and name ordered by score descending
+SELECT score, name FROM second_table ORDER BY score DESC;
