1e3a02961435c286464cc7559005afc21664d3b1
kayanabdullah00-eng <kayanabdullah00@gmail.com>
2026-07-22 18:08:35 +0300

    k

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python3
-"""Lists all cities from the database hbtn_0e_4_usa."""
-import sys
+"""Lists all states with a name starting with N (upper N)."""
 import MySQLdb
+import sys
 
 
 if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
+        user=username,
+        passwd=password,
+        db=db_name
     )
+
     cursor = db.cursor()
-    cursor.execute("SELECT cities.id, cities.name, states.name "
-                   "FROM cities JOIN states ON cities.state_id = states.id "
-                   "ORDER BY cities.id ASC")
-    for row in cursor.fetchall():
+    cursor.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
+    )
+    rows = cursor.fetchall()
+    for row in rows:
         print(row)
+
     cursor.close()
     db.close()
