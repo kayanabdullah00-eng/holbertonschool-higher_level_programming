df91b0013726b5eb53df7e924a9963882afc7ea0
kayanabdullah00-eng <kayanabdullah00@gmail.com>
2026-07-22 18:06:11 +0300

    k

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,39 +1,22 @@
 #!/usr/bin/python3
-Tis module connects to a MySQL database and lists all states
-with  name starting with N (upper N) from the database hbtn_0e_0_usa.
-""
+"""Lists all cities from the database hbtn_0e_4_usa."""
 import sys
 import MySQLdb
 
-if __name__ == "__main__":
-    # 1. قراءة المدخلات
-    user_name = sys.argv[1]
-    user_password = sys.argv[2]
-    db_name = sys.argv[3]
 
-    # 2. إنشاء الاتصال بقاعدة البيانات
+if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=user_name,
-        passwd=user_password,
-        db=db_name
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
-
-    # 3. استعلام الـ JOIN لربط المدن بالولايات
     cursor = db.cursor()
-    cursor.execute("""
-        SELECT cities.id, cities.name, states.name
-        FROM cities
-        JOIN states ON cities.state_id = states.id
-        ORDER BY cities.id ASC
-    """)
-
-    # 4. جلب وطباعة النتائج
-    query_rows = cursor.fetchall()
-    for row in query_rows:
+    cursor.execute("SELECT cities.id, cities.name, states.name "
+                   "FROM cities JOIN states ON cities.state_id = states.id "
+                   "ORDER BY cities.id ASC")
+    for row in cursor.fetchall():
         print(row)
-
-    # 5. إغلاق الاتصال
     cursor.close()
     db.close()
