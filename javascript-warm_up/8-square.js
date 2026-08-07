caac6ff1735177a1cf95610a117e15cf41894c63
kayanabdullah00-eng <kayanabdullah00@gmail.com>
2026-08-08 01:20:20 +0300

    Add square script

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const size = parseInt(process.argv[2]);
+
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  let row = 0;
+
+  while (row < size) {
+    console.log('X'.repeat(size));
+    row++;
+  }
+}
