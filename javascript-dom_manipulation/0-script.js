50a0349c5f88e9b4d5c1a518de93af66e5ba9a3c
kayanabdullah00-eng <kayanabdullah00@gmail.com>
2026-08-10 17:16:08 +0300

    k

--- javascript-dom_manipulation/0-script.js
+++ javascript-dom_manipulation/0-script.js
@@ -1,3 +1,3 @@
-const headerElement =document.querySelector('header');
+const headerElement = document.querySelector('header');
 
-headerElement.style.color ='#FF0000';
+headerElement.style.color = '#FF0000';
